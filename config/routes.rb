336564b616b634cb4addf1e96daf3ea33e89e986
David Estes <destes@redwindsw.com>
2012-01-20 14:43:05 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+   namespace :spud do
+   end
+   
+end
+
---
+++ lib/spud_admin/engine.rb
@@ -0,0 +1,12 @@
+require 'spud_admin'
+require 'rails'
+require 'action_controller'
+require 'rubygems'
+require 'dynamic_form'
+require 'breadcrumbs_on_rails'
+require 'will_paginate'
+module SpudAdmin 
+ class Engine < Rails::Engine
+     engine_name :spud_admin
+ end
+end
